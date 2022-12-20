9d7c73502f2e1137964a99c931820f1508eb7737
wa5ina <ren-nasr@student.1337.ma>
2022-12-20 20:10:56 +0100

    add, substract, multiply and devide in finite field

--- src/crypto/field.rs
+++ src/crypto/field.rs
@@ -1,28 +1,73 @@
 pub struct FieldElement {
-    num: u8,
-
-    prime: u8,
+    num: u128,
+    prime: u128,
 }
+
 impl FieldElement {
-    pub fn new(num: u8, prime: u8) -> FieldElement {
+    pub fn new(num: u128, prime: u128) -> FieldElement {
         if num >= prime || prime <= 0 {
             panic!("Num {} not in field range 0 to {}", num, prime - 1);
         }
         FieldElement { num, prime }
     }
-
-    pub fn num(&self) -> u8 {
+    pub fn num(&self) -> u128 {
         self.num
     }
-
-    pub fn prime(&self) -> u8 {
+    pub fn prime(&self) -> u128 {
         self.prime
     }
-
     pub fn eq(&self, other: &FieldElement) -> bool {
         if self.prime != other.prime {
             panic!("Cannot compare two numbers in different Fields");
         }
         self.num == other.num
     }
+    pub fn add(&self, other: &FieldElement) -> FieldElement {
+        if self.prime != other.prime {
+            panic!("Cannot add two numbers in different Fields");
+        }
+        FieldElement {
+            num: (self.num + other.num) % self.prime,
+            prime: (self.prime),
+        }
+    }
+    pub fn substract(&self, other: &FieldElement) -> FieldElement {
+        if self.prime != other.prime {
+            panic!("Cannot substract two numbers in different Fields");
+        }
+        FieldElement {
+            num: (self.num + self.prime - other.num) % self.prime,
+            prime: (self.prime),
+        }
+    }
+    pub fn multiply(&self, other: &FieldElement) -> FieldElement {
+        if self.prime != other.prime {
+            panic!("Cannot multiply two numbers in different Fields");
+        }
+        FieldElement {
+            num: (self.num * other.num) % self.prime,
+            prime: (self.prime),
+        }
+    }
+    pub fn pow(&self, exponent: u128) -> FieldElement {
+        let mut result = FieldElement::new(1, self.prime);
+        let mut base = self.clone();
+        let mut exp = exponent;
+        //
+        for _ in 0..exp {
+            result = result.multiply(&base)
+        }
+        result
+    }
+    pub fn divide(&self, other: &FieldElement) -> FieldElement {
+        if self.prime != other.prime {
+            panic!("Cannot divide two numbers in different Fields");
+        }
+        // use Fermat's little theorem
+        // self.num**(p-1) % p == 1
+        let mut other_inv = other.pow(self.prime - 2);
+        self.multiply(&other_inv)
+    }
+
+
 }
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,3 @@
-
 mod crypto;
 use crypto::field::FieldElement;
 
@@ -6,7 +5,21 @@ fn main() {
     let p = FieldElement::new(7, 13);
     let q = FieldElement::new(6, 13);
     println!("p.num = {}", p.num());
+    println!("q.num = {}", q.num());
     println!("p.prime = {}", p.prime());
     println!("p == q is {}", p.eq(&q));
-}
+    let res = p.add(&q);
+    println!("p + q = {}", res.num());
+    // substracing q - p
+    let res2 = q.substract(&p);
+    println!("q - p = {}", res2.num());
 
+    let p1 = FieldElement::new(3, 13);
+    let p2 = FieldElement::new(12, 13);
+    let res3 = FieldElement::new(10, 13);
+
+    assert!(p1.multiply(&p2).eq(&res3));
+    println!("p1 * p2 = {}", p1.multiply(&p2).num());
+    println!("p1 * p2 = {}", res3.num());
+    println!("p1.pow(3) = {}", p1.pow(3).num());
+}
